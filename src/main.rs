18fe2de0d88f138dff36f47d05129f361386f5c3
Renan Barroso <barrosoren@gmail.com>
2025-07-07 09:21:58 +0300

    replace game name

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Bevy game".to_string(), // ToDo
+                        title: "Lunar Lander".to_string(),
                         // Bind to canvas included in `index.html`
                         canvas: Some("#bevy".to_owned()),
                         fit_canvas_to_parent: true,
